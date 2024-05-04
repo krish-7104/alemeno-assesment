efdcd17a0a41cd56bc313280266430f5e92324f1
Krish Jotaniya <krishjotaniya71@gmail.com>
2024-05-04 21:30:34 +0530

    Mark completed added optimistic update

---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,90 @@
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { db } from "../firebase/config";
+import { UserIcon } from "./UserIcon";
+import Progress from "./Progress";
+
+const ProfileCard = ({ course: initialCourse }) => {
+  const { user, id } = useSelector((state) => state.user);
+  const [course, setCourse] = useState(initialCourse);
+
+  const MarkCompleted = async (courseId) => {
+    if (!id || !courseId) return;
+    try {
+      const studentRef = doc(db, "students", id);
+      const studentDoc = await getDoc(studentRef);
+      if (studentDoc.exists()) {
+        const courses = studentDoc.data().courses || [];
+        const index = courses.findIndex(
+          (course) => course.courseId === courseId
+        );
+        if (index !== -1) {
+          courses[index].completed = true;
+          courses[index].progress = 100;
+          await updateDoc(studentRef, { courses });
+          setCourse({ ...course, completed: true, progress: 100 });
+        } else {
+          console.error("Course not found in student's courses array.");
+        }
+      } else {
+        console.error("Student document does not exist.");
+      }
+    } catch (error) {
+      console.error("Error updating course data:", error);
+    }
+  };
+
+  return (
+    <div
+      key={course.id}
+      className="bg-white shadow-md rounded-md cursor-pointer relative"
+    >
+      <div className="md:h-[200px] rounded-t-md group overflow-hidden">
+        <img
+          src={course.thumbnail}
+          className="w-full object-cover rounded-t-md group-hover:scale-105 transition-animate"
+          alt="Course Thumbnail"
+        />
+      </div>
+      <div className="px-2 mb-5">
+        <p className="mt-2 text-lg font-semibold">{course.name}</p>
+        <p className="text-sm text-slate-600 flex justify-start mt-1 items-center">
+          <UserIcon /> {course.instructor}
+        </p>
+        <p className="text-sm my-4 text-slate-600 flex justify-start items-center">
+          <Progress progress={course.progress} />
+          <p className="mx-2">{course.progress}%</p>
+        </p>
+        {course.completed ? (
+          <button
+            className="rounded-md mx-auto bg-slate-600 bg-gradient-to-br shadow-md  w-full px-4 py-2 text-white text-sm font-medium"
+            onClick={() => MarkCompleted(course.id)}
+          >
+            Course Completed
+          </button>
+        ) : (
+          <button
+            className="rounded-md mx-auto from-indigo-600 to-indigo-500 bg-gradient-to-br shadow-md shadow-indigo-400/40 w-full px-4 py-2 text-white text-sm font-medium"
+            onClick={() => MarkCompleted(course.id)}
+          >
+            Mark As Completed
+          </button>
+        )}
+      </div>
+      <span
+        className={`px-3 py-1 text-xs rounded-full font-medium ${
+          course.enrollmentStatus === "Open"
+            ? "bg-green-600"
+            : course.enrollmentStatus === "In Progress"
+            ? "bg-indigo-600"
+            : "bg-red-600"
+        } text-white absolute top-4 right-4`}
+      >
+        {course.enrollmentStatus}
+      </span>
+    </div>
+  );
+};
+
+export default ProfileCard;
--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -1,24 +1,16 @@
 import { useSelector } from "react-redux";
 import CourseCard from "../components/CourseCard";
 import { useEffect, useState } from "react";
-import {
-  collection,
-  doc,
-  getDoc,
-  getDocs,
-  updateDoc,
-} from "firebase/firestore";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 import { db } from "../firebase/config";
 import { Link, useNavigate } from "react-router-dom";
-import { UserIcon } from "../components/UserIcon";
-import { GiProgression } from "react-icons/gi";
-import Progress from "../components/Progress";
+import ProfileCard from "../components/ProfileCard";
 
 const Profile = () => {
   const { user, id } = useSelector((state) => state.user);
   const navigate = useNavigate();
   const [profileData, setProfileData] = useState([]);
-
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     if (!id) navigate("/");
   }, [id, navigate, user]);
@@ -57,38 +49,16 @@ const Profile = () => {
             }
           });
           setProfileData(mergedCourses);
+          setLoading(false);
         }
       } catch (error) {
+        setLoading(false);
         console.error("Error fetching profile data:", error);
       }
     };
 
     fetchCourses();
   }, [id]);
-
-  const MarkCompleted = async (courseId) => {
-    if (!id || !courseId) return;
-    try {
-      const studentRef = doc(db, "students", id);
-      const studentDoc = await getDoc(studentRef);
-      if (studentDoc.exists()) {
-        const courses = studentDoc.data().courses || [];
-        const index = courses.findIndex(
-          (course) => course.courseId === courseId
-        );
-        if (index !== -1) {
-          courses[index].completed = true;
-          await updateDoc(studentRef, { courses });
-        } else {
-          console.error("Course not found in student's courses array.");
-        }
-      } else {
-        console.error("Student document does not exist.");
-      }
-    } catch (error) {
-      console.error("Error updating course data:", error);
-    }
-  };
 
   return (
     <section className="pt-28 max-w-6xl mx-auto">
@@ -116,56 +86,10 @@ const Profile = () => {
       )}
       <div className="md:grid md:grid-cols-3 gap-6 w-[90%] mx-auto my-10 flex flex-wrap">
         {profileData.map((course) => (
-          <div
-            key={course.id}
-            className="bg-white shadow-md rounded-md cursor-pointer relative"
-          >
-            <div className="md:h-[200px] rounded-t-md group overflow-hidden">
-              <img
-                src={course.thumbnail}
-                className="w-full object-cover rounded-t-md group-hover:scale-105 transition-animate"
-              />
-            </div>
-            <div className="px-2 mb-5">
-              <p className="mt-2 text-lg font-semibold">{course.name}</p>
-              <p className="text-sm text-slate-600 flex justify-start mt-1 items-center">
-                <UserIcon /> {course.instructor}
-              </p>
-              <p className="text-sm my-4 text-slate-600 flex justify-start items-center">
-                <Progress progress={course.progress} />
-                <p className="mx-2">{course.progress}%</p>
-              </p>
-              {course.completed ? (
-                <button
-                  className="rounded-md mx-auto bg-slate-600 bg-gradient-to-br shadow-md  w-full px-4 py-2 text-white text-sm font-medium"
-                  onClick={() => MarkCompleted(course.id)}
-                >
-                  Course Completed
-                </button>
-              ) : (
-                <button
-                  className="rounded-md mx-auto from-indigo-600 to-indigo-500 bg-gradient-to-br shadow-md shadow-indigo-400/40 w-full px-4 py-2 text-white text-sm font-medium"
-                  onClick={() => MarkCompleted(course.id)}
-                >
-                  Mark As Completed
-                </button>
-              )}
-            </div>
-            <span
-              className={`px-3 py-1 text-xs rounded-full font-medium ${
-                course.enrollmentStatus === "Open"
-                  ? "bg-green-600"
-                  : course.enrollmentStatus === "In Progress"
-                  ? "bg-indigo-600"
-                  : "bg-red-600"
-              } text-white absolute top-4 right-4`}
-            >
-              {course.enrollmentStatus}
-            </span>
-          </div>
+          <ProfileCard key={course.id} course={course} />
         ))}
       </div>
-      {profileData.length === 0 && (
+      {!loading && profileData.length === 0 && (
         <div className="flex justify-center items-center flex-col">
           <p className="text-center font-medium text-lg text-slate-700">
             No Enrolled Courses Found!
